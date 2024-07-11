d4239ef6122382ac940de6c80651103fc0c53528
Rosana <annros2003@gmail.com>
2024-07-11 10:57:19 -0700

    configured connection to db and created tables structure

--- LibraryManager/src/main/java/com/testTask/LibraryManager/model/Author.java
+++ LibraryManager/src/main/java/com/testTask/LibraryManager/model/Author.java
@@ -1,4 +1,42 @@
 package com.testTask.LibraryManager.model;
 
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
 public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long authorID;
+    private String name;
+    private String bio;
+
+    public Author() {
+    }
+
+    public Long getAuthorID() {
+        return authorID;
+    }
+
+    public void setAuthorID(Long authorID) {
+        this.authorID = authorID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
 }
--- LibraryManager/src/main/java/com/testTask/LibraryManager/model/Book.java
+++ LibraryManager/src/main/java/com/testTask/LibraryManager/model/Book.java
@@ -1,4 +1,77 @@
 package com.testTask.LibraryManager.model;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookID;
+    private Long authorID;
+    private String title;
+    private String genre;
+    private String description;
+    private Date publishedDate; // or imported from java.sql
+    private int pages;
+
+    public Book() {
+    }
+
+    public void setBookID(Long bookID) {
+        this.bookID = bookID;
+    }
+
+    public Long getBookID() {
+        return bookID;
+    }
+
+    public Long getAuthorID() {
+        return authorID;
+    }
+
+    public void setAuthorID(Long authorID) {
+        this.authorID = authorID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getPublishedDate() {
+        return publishedDate;
+    }
+
+    public void setPublishedDate(Date publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+
+    public int getPages() {
+        return pages;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
 }
