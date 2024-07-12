c2c811a4dab62a9c7efe493df1d90c2174d14230
Rosana <annros2003@gmail.com>
2024-07-12 06:56:35 -0700

    added repository methods

--- LibraryManager/src/main/java/com/testTask/LibraryManager/repository/AuthorRepository.java
+++ LibraryManager/src/main/java/com/testTask/LibraryManager/repository/AuthorRepository.java
@@ -1,4 +1,12 @@
 package com.testTask.LibraryManager.repository;
+import com.testTask.LibraryManager.model.Author;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface AuthorRepository {
+import java.util.List;
+
+@Repository
+public interface AuthorRepository extends JpaRepository<Author, Long> {
+//    List<Author> findAuthorByAuthorName(String authorName);
+    boolean existsByName(String name);
 }
--- LibraryManager/src/main/java/com/testTask/LibraryManager/repository/BookRepository.java
+++ LibraryManager/src/main/java/com/testTask/LibraryManager/repository/BookRepository.java
@@ -1,4 +1,14 @@
 package com.testTask.LibraryManager.repository;
 
-public interface BookRepository {
+import com.testTask.LibraryManager.model.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+    List<Book> findBooksByTitle(String title);
+    List<Book> findBooksByAuthor_Name(String author);
+    boolean existsByTitle(String title);
 }
