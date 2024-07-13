4560079f30ba3bd7d63d94cb0c99d4866d2b2b09
rosana <annros2003@gmail.com>
2024-07-14 01:01:32 +0300

    solved a bug: TypeError: NetworkError when attempting to fetch resource  by adding @CrossOrigin

--- LibraryManager/src/main/java/com/testTask/LibraryManager/controller/LibraryManagerController.java
+++ LibraryManager/src/main/java/com/testTask/LibraryManager/controller/LibraryManagerController.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/library")
+@CrossOrigin
 public class LibraryManagerController {
     @Autowired
     private LibraryManagerService libManager;
