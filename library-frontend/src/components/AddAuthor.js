2e90723dba11d303c421b242aaaf01434d2135de
rosana <annros2003@gmail.com>
2024-07-14 21:57:50 +0300

    implemented addAuthor

--- library-frontend/src/components/AddAuthor.js
+++ library-frontend/src/components/AddAuthor.js
@@ -1,12 +1,55 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { addAuthor } from "../utils/AuthorService"
+
 export function AddAuthor() {
+    let navigate = useNavigate()
+
+    const [author, setAuthor] = useState({
+        name: '',
+        bio: ''
+    })
+
+    const handleChange = (event) => {
+        const {name, value} = event.target
+        setAuthor((prevAuthor) => ({
+            ...prevAuthor,
+            [name]: value,
+        }))
+    }
+
+    const handleSubmitButton = async (event) => {
+        event.preventDefault()
+        try {
+            const result = await addAuthor(author)
+            alert(result)
+            let path = "/"
+            navigate(path)
+        } catch (error) {
+            alert(error)
+        }
+    }
+
     return (
         <div className="form-container">
             <h2 className="plain-heading">Add an author</h2>
-            <form className="classic-form">
+            <form className="classic-form" onSubmit={handleSubmitButton}>
                 <label for="name">Name:</label>
-                <input type="text" id="name" name="name" required /><br></br>
+                <input
+                    type="text"
+                    id="name"
+                    name="name"
+                    onChange={handleChange}
+                    required
+                /><br></br>
+
                 <label for="bio">Bio:</label>
-                <input type="text" id="bio" name="bio" required /><br></br>
+                <input
+                    type="text"
+                    id="bio" name="bio"
+                    onChange={handleChange}
+                    required
+                /><br></br>
 
                 <input type="submit" value="Add"></input>
             </form>
--- library-frontend/src/utils/AuthorService.js
+++ library-frontend/src/utils/AuthorService.js
@@ -16,3 +16,28 @@ export const fetchAuthors = async () => {
         throw error
     }
 }
+
+export const addAuthor = async (author) => {
+    try {
+        const response = await fetch('http://localhost:8080/library/addAuthor', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(author),
+        });
+
+        const responseBody = await response.text()
+        if (!response.ok) {
+            throw new Error("Cannot add an author! " + response)
+        }
+        if (responseBody !== "New author has been added") { 
+            throw new Error("Cannot add an author! " + responseBody)
+        }
+        
+        return await "Author has been added. " + " Status: " + response.status
+    } catch (error) {
+        console.error(error)
+        throw error
+    }
+}
