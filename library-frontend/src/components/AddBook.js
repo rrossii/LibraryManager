d98e9126401c6122e544246cc791e091430ea539
rosana <annros2003@gmail.com>
2024-07-14 21:50:16 +0300

    implemented addBook

--- library-frontend/src/components/AddBook.js
+++ library-frontend/src/components/AddBook.js
@@ -1,22 +1,117 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import { addBook } from "../utils/BookService"
+
 export function AddBook() {
+    let navigate = useNavigate();
+
+    const [book, setBook] = useState({
+        title: '',
+        genre: '',
+        description: '',
+        publishedYear: 0,
+        pages: 0,
+        author: {
+            name: '',
+            bio: 'Some biography',
+        },
+    });
+
+    const handleBookChange = (event) => {
+        const { name, value } = event.target
+        setBook((prevBook) => ({
+            ...prevBook,
+            [name]: value,
+        }))
+    }
+
+    const handleAuthorChange = (event) => {
+        const { name, value } = event.target
+        setBook((prevBook) => ({
+            ...prevBook,
+            author: {
+                ...prevBook.author,
+                [name]: value,
+            },
+        }))
+    }
+
+    const handleSubmitButton = async (event) => {
+        event.preventDefault()
+        try {
+            const result = await addBook(book)
+            alert(result)
+            let path = "/"
+            navigate(path)
+        } catch (error) {
+            alert(error)
+        }
+    }
+
     return (
         <div className="form-container">
             <h2 className="plain-heading">Add a book</h2>
-            <form className="classic-form">
+            <form className="classic-form" onSubmit={handleSubmitButton}>
                 <label for="title">Title:</label>
-                <input type="text" id="title" name="title" required /><br></br>
-                <label for="author">Author's name:</label>
-                <input type="text" id="author" name="author" required /><br></br>
-                <label for="genre">Genre:</label>
-                <input type="text" id="genre" name="genre" required /><br></br>
-                <label for="description">Description:</label>
-                <input type="text" id="description" name="description" required /><br></br>
-                <label for="year">Published year:</label>
-                <input type="number" id="year" name="year" min ="0" max="2026" required /><br></br> 
-                <label for="pages">Pages:</label>
-                <input type="number" id="pages" name="pages" min ="0" required /><br></br> 
+                <input
+                    type="text"
+                    id="title"
+                    name="title"
+                    required 
+                    onChange={handleBookChange}
+                /><br></br>
 
-                <input type="submit" value="Add"></input>
+                <label for="author">Author's name:</label>
+                <input
+                    type="text"
+                    id="author"
+                    name="author"
+                    required 
+                    onChange={handleAuthorChange}
+                /><br></br>
+
+                <label for="genre">Genre:</label>
+                <input
+                    type="text"
+                    id="genre"
+                    name="genre" 
+                    required
+                    onChange={handleBookChange}
+                /><br></br>
+
+                <label for="description">Description:</label>
+                <input
+                    type="text"
+                    id="description"
+                    name="description"
+                    required 
+                    onChange={handleBookChange}
+                /><br></br>
+
+                <label for="year">Published year:</label>
+                <input
+                    type="number"
+                    id="year"
+                    name="year"
+                    min="0" max="2026"
+                    required 
+                    onChange={handleBookChange}
+                /><br></br>
+
+                <label for="pages">Pages:</label>
+                <input
+                    type="number"
+                    id="pages"
+                    name="pages"
+                    min="0"
+                    required 
+                    onChange={handleBookChange}
+                /><br></br>
+
+                <input
+                    type="submit"
+                    value="Add">
+                </input>
             </form>
         </div>
     )
--- library-frontend/src/utils/BookService.js
+++ library-frontend/src/utils/BookService.js
@@ -1,3 +1,4 @@
+
 export const fetchBooks = async () => {
     try {
         const response = await fetch("http://localhost:8080/library/allBooks", {
@@ -13,6 +14,32 @@ export const fetchBooks = async () => {
         return await response.json()
     } catch (error) {
         console.log(error)
+        throw error
+    }
+}
+
+export const addBook = async (book) => {
+    try {
+        const response = await fetch('http://localhost:8080/library/addBook', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(book),
+        });
+
+        const responseBody = await response.text()
+        if (!response.ok) {
+            throw new Error("Cannot add book! " + response)
+        }
+        if (responseBody !== "New book has been added") { // з беку відповідь (колись зробити нормально через ResponseEntity :))
+            throw new Error("Cannot add book! " + responseBody)
+        }
+        
+        return await "Book has been added. " + " Status: " + response.status
+
+    } catch (error) {
+        console.error(error)
         throw error
     }
 }
